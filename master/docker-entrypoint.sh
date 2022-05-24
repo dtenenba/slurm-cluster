2ee7f99ace8e4513c3e693110cf870bd4cb5cedc
dtenenba <dtenenba@fhcrc.org>
2022-05-23 22:22:00 -0700

    head node changes mostly done

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -5,4 +5,8 @@ sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
 sudo service munge start
 sudo service slurmctld start
 
+###
+sudo /opt/ood/ood-portal-generator/sbin/update_ood_portal && sudo service apache2 restart
+###
+
 tail -f /dev/null
