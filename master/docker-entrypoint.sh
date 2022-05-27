d34ab815bf39a546ee3ed2b09d73e11e2d4cfdee
dtenenba <dtenenba@fhcrc.org>
2022-05-26 23:52:13 -0700

    added node dependencies

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
+whoami > /tmp/whoami.txt
+
 sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
 
 sudo service munge start
 sudo service slurmctld start
+
+# sudo rm -rf /home/admin/ondemand/dev
+# sudo chown -R admin:admin /user_apps/admin
+# sudo ln -s /user_apps/admin /home/admin/ondemand/dev
+
+git -C ~/ondemand/dev clone https://github.com/dtenenba/bc_osc_example_shiny.git || true
+git -C ~/ondemand/dev clone https://github.com/dtenenba/bc_osc_rstudio_server.git || true
+git -C ~/ondemand/dev clone https://github.com/dtenenba/bc_example_jupyter.git || true
+git -C ~/ondemand/dev clone https://github.com/dtenenba/flask_passenger_wsgi_hello.git || true
+git -C ~/ondemand/dev clone https://github.com/dtenenba/bc_example_flaskapp.git || true
+
+git -C ~/ondemand/dev/bc_osc_example_shiny pull || true
+git -C ~/ondemand/dev/bc_osc_rstudio_server pull || true
+git -C ~/ondemand/dev/bc_example_jupyter pull || true
+git -C ~/ondemand/dev/flask_passenger_wsgi_hello pull || true
+git -C ~/ondemand/dev/bc_example_flaskapp pull || true
+
+
+
 
 ###
 sudo /opt/ood/ood-portal-generator/sbin/update_ood_portal && sudo service apache2 restart
--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -5,4 +5,9 @@ sudo sed -i "s/REPLACE_IT/CPUs=$(nproc)/g" /etc/slurm-llnl/slurm.conf
 sudo service munge start
 sudo slurmd -N $(hostname)
 
+# sudo rm -rf /home/admin/ondemand/dev
+# sudo chown -R admin:admin /user_apps/admin
+# sudo ln -s /user_apps/admin /home/admin/ondemand/dev
+
+
 tail -f /dev/null
